143c153df44592544c710139894c8c69ccde77fd
Fedenko Maksym <feden2906@gmail.com>
2021-01-16 23:08:27 +0200

    70% lesson 2

--- src/components/film-list/FilmList.js
+++ src/components/film-list/FilmList.js
@@ -3,7 +3,6 @@ import styles from './FilmList.module.css'
 
 export const FilmList = ({items, onFilmsClick}) => {
 
-
   return (
       <div className={styles.wrapper}>
         {items.map(item => (<div onClick={() => onFilmsClick(item)} className={styles.itemWrapper} key={item.id}>
