bd44de799082f6a966942fc5394b0b3650fe95db
Fedenko Maksym <feden2906@gmail.com>
2021-01-18 23:58:40 +0200

    styled

--- src/components/pagination-wrapper/PaginationWrapper.js
+++ src/components/pagination-wrapper/PaginationWrapper.js
@@ -1,34 +1,42 @@
 // import styles from './Pagination.module.css'
 
-export const PaginationWrapper = ({children, currentPage, totalPage, onPrevClick, onNextClick, handlerLastPage, handlerFirstPage}) => {
+export const PaginationWrapper = ({
+                                    children,
+                                    currentPage,
+                                    totalPage,
+                                    onPrevClick,
+                                    onNextClick,
+                                    handlerLastPage,
+                                    handlerFirstPage
+                                  }) => {
 
-    const handlerPrevClick = () => {
-        if (currentPage - 1 > 0) {
-            onPrevClick(currentPage -1)
-        }
+  const handlerPrevClick = () => {
+    if (currentPage - 1 > 0) {
+      onPrevClick(currentPage - 1)
     }
-    const handlerNextClick = () => {
-        if (currentPage + 1 <= totalPage) {
-            onNextClick(currentPage +1)
-        }
+  }
+  const handlerNextClick = () => {
+    if (currentPage + 1 <= totalPage) {
+      onNextClick(currentPage + 1)
     }
-    const handlerFirstClick = () => {
-        handlerFirstPage && handlerFirstPage(1)
-    }
-    const handlerLastClick = () => {
-        handlerLastPage && handlerLastPage(totalPage)
-    }
+  }
+  const handlerFirstClick = () => {
+    handlerFirstPage && handlerFirstPage(1)
+  }
+  const handlerLastClick = () => {
+    handlerLastPage && handlerLastPage(totalPage)
+  }
 
-    return (
+  return (
+      <div>
         <div>
-            <div>
-                <button disabled={currentPage === 1} onClick={handlerFirstClick}>first page</button>
-                <button disabled={currentPage - 1 <= 0} onClick={handlerPrevClick}>prev page</button>
-                <span>{currentPage} of {totalPage}</span>
-                <button disabled={currentPage + 1 > totalPage} onClick={handlerNextClick}>next page</button>
-                <button disabled={currentPage === totalPage} onClick={handlerLastClick}>last page</button>
-            </div>
-            {children}
+          <button disabled={currentPage === 1} onClick={handlerFirstClick}>first page</button>
+          <button disabled={currentPage - 1 <= 0} onClick={handlerPrevClick}>prev page</button>
+          <span>{currentPage} of {totalPage}</span>
+          <button disabled={currentPage + 1 > totalPage} onClick={handlerNextClick}>next page</button>
+          <button disabled={currentPage === totalPage} onClick={handlerLastClick}>last page</button>
         </div>
-    );
+        {children}
+      </div>
+  );
 }
